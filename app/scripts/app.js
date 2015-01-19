58775fbaf9684b7adc49d8ba4bdba163275d4775
diffcunha <diogo.f.cunha@gmail.com>
2015-01-19 00:01:44 +0000

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angular-addressbook
+ * @description
+ * # angular-addressbook
+ *
+ * Main module of the angular-addressbook application.
+ */
+angular
+  .module('angular-addressbook', []);
